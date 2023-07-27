f5f446710c0a7bf1ffe35e82a28341cf430754d3
Will Walter <waltwilo@amazon.com>
2023-07-27 13:20:24 -0600

    Added docker archival tests and pruned unused fields

--- test/java/io/jenkins/plugins/awsinspectorbuildstep/sbomparsing/SeverityTest.java
+++ test/java/io/jenkins/plugins/awsinspectorbuildstep/sbomparsing/SeverityTest.java
@@ -3,7 +3,7 @@ package io.jenkins.plugins.awsinspectorbuildstep.sbomparsing;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SeverityTest {
     @Test
