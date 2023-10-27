603d032094a711428de10cfba992aad8a105109e
Will Walter <waltwilo@amazon.com>
2023-10-27 13:25:39 -0600

    Switching to URLEncoder for sanitization

--- src/main/java/io/jenkins/plugins/amazoninspectorbuildstep/utils/Sanitizer.java
+++ src/main/java/io/jenkins/plugins/amazoninspectorbuildstep/utils/Sanitizer.java
@@ -1,27 +1,16 @@
 package io.jenkins.plugins.amazoninspectorbuildstep.utils;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class Sanitizer {
     private Sanitizer() {}
 
-    public static String sanitizeUrl(String rawUrl) throws MalformedURLException, URISyntaxException {
-        URL url = new URL(rawUrl);
-        URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
-        String sanitizedUrl = uri.toURL().toString();
-        sanitizedUrl = sanitizedUrl.replace("'", "%27");
-        sanitizedUrl = sanitizedUrl.replace(";", "%3B");
-        sanitizedUrl = sanitizedUrl.replace("(", "%28");
-        sanitizedUrl = sanitizedUrl.replace(")", "%29");
-        sanitizedUrl = sanitizedUrl.replace("=", "%3D");
-
-        return sanitizedUrl.replace("file:/", "file:///");
+    public static String sanitizeUrl(String rawUrl) {
+        return URLEncoder.encode(rawUrl, StandardCharsets.UTF_8).replace("%2F", "/");
     }
 
-    public static String sanitizeNonUrl(String text) throws MalformedURLException, URISyntaxException {
-        return sanitizeUrl("file://" + text).replace("file:///", "/");
+    public static String sanitizeNonUrl(String text) {
+        return sanitizeUrl(text).replace("%3A", ":");
     }
 }
