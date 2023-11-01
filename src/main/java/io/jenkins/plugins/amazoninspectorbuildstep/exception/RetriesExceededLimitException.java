e950d777250327bcccebe37af7c136c2f78339c6
Will Walter <waltwilo@amazon.com>
2023-11-01 11:35:42 -0600

    Core package integration
    cr: https://code.amazon.com/reviews/CR-105216871

--- src/main/java/io/jenkins/plugins/amazoninspectorbuildstep/exception/RetriesExceededLimitException.java
+++
@@ -1,7 +0,0 @@
-package io.jenkins.plugins.amazoninspectorbuildstep.exception;
-
-public class RetriesExceededLimitException extends Exception {
-    public RetriesExceededLimitException(String errorMessage) {
-        super(errorMessage);
-    }
-}
