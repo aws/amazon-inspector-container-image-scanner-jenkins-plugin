bb9e1ee390030ad2fc00c2d77b534a94dea5d50a
Will Walter <waltwilo@amazon.com>
2024-01-16 12:39:13 -0700

    Removing unused function

--- src/main/java/com/amazon/inspector/jenkins/amazoninspectorbuildstep/sbomgen/SbomgenDownloader.java
+++ src/main/java/com/amazon/inspector/jenkins/amazoninspectorbuildstep/sbomgen/SbomgenDownloader.java
@@ -115,28 +115,4 @@ public class SbomgenDownloader {
 
         return sbomgenPath;
     }
-
-    private static String copyBinary(String src, String dest) throws IOException {
-        File copied = new File(dest);
-
-        try (
-                InputStream in = new BufferedInputStream(
-                        new FileInputStream(src));
-                OutputStream out = new BufferedOutputStream(
-                        new FileOutputStream(copied))) {
-
-            byte[] buffer = new byte[1024];
-            int lengthRead;
-            while ((lengthRead = in.read(buffer)) > 0) {
-                out.write(buffer, 0, lengthRead);
-                out.flush();
-            }
-        } catch (IOException e) {
-            throw e;
-        }
-
-        copied.setExecutable(true);
-
-        return dest;
-    }
 }
