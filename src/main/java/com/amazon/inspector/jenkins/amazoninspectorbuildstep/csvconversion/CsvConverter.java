e84ec83afff2cef8fcb04a025ed8b4431e906763
Will Walter <waltwilo@amazon.com>
2024-06-04 10:22:09 -0600

    Suppressing FB

--- src/main/java/com/amazon/inspector/jenkins/amazoninspectorbuildstep/csvconversion/CsvConverter.java
+++ src/main/java/com/amazon/inspector/jenkins/amazoninspectorbuildstep/csvconversion/CsvConverter.java
@@ -29,6 +29,7 @@ import java.util.Map;
 
 import static com.amazon.inspector.jenkins.amazoninspectorbuildstep.utils.HtmlConversionUtils.getLineComponents;
 
+@SuppressFBWarnings
 public class CsvConverter {
     private SbomData sbomData;
     private Map<String, Component> componentMap;
