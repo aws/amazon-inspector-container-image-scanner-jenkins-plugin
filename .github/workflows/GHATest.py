8f206b314417b4dc8f85c32bde6c728c2e3a389f
waltwilo <144280698+waltwilo@users.noreply.github.com>
2024-07-23 13:58:59 -0600

    Create GHATest.py

---
+++ .github/workflows/GHATest.py
@@ -0,0 +1,18 @@
+import argparse
+import subprocess
+import sys
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-a')    
+parser.add_argument('-p')  
+parser.add_argument('-l') 
+
+args = parser.parse_args()
+print(args)
+
+test = subprocess.run(["ssh", "-o", "StrictHostKeyChecking=no", "-i", "~/.ssh/id_rsa", "-l", args.l, "-p", args.p, args.a, "build", "test", "-s"], capture_output=True, text=True) 
+print(test.stdout)
+
+if "SUCCESS" not in test.stdout:
+  sys.exit(1)
