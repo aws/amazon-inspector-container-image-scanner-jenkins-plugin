f66408b7ad80345cd056937097dfeeb6f9c8b120
Will Walter <waltwilo@amazon.com>
2024-08-14 13:34:20 -0600

    Moving callable to package-protected class

--- src/main/java/com/amazon/inspector/jenkins/amazoninspectorbuildstep/sbomgen/SbomgenDownloader.java
+++ src/main/java/com/amazon/inspector/jenkins/amazoninspectorbuildstep/sbomgen/SbomgenDownloader.java
@@ -1,6 +1,5 @@
 package com.amazon.inspector.jenkins.amazoninspectorbuildstep.sbomgen;
 
-import com.amazon.inspector.jenkins.amazoninspectorbuildstep.AmazonInspectorBuilder;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.FilePath;
 import hudson.remoting.VirtualChannel;
@@ -19,7 +18,6 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class SbomgenDownloader {
-
     public static String getBinary(String configInput, FilePath workspace) throws IOException, InterruptedException, ExecutionException {
         String url = getUrl(configInput);
         FilePath zipPath = downloadFile(url, workspace);
@@ -53,6 +51,68 @@ public class SbomgenDownloader {
         return sbomgenZip;
     }
 
+
+    @SuppressFBWarnings()
+    private static String unzipFile(FilePath zip) throws IOException, InterruptedException, ExecutionException {
+        FilePath destination = zip.getParent().child(zip.getRemote().replace(".zip", ""));
+        Future<String> callable = zip.actAsync(new DownloaderCallable(destination.getRemote()));
+
+        return callable.get();
+    }
+}
+
+class DownloaderCallable implements FilePath.FileCallable {
+    String destinationPath;
+
+    DownloaderCallable(String destinationPath) {
+        this.destinationPath = destinationPath;
+    }
+
+    @Override
+    public void checkRoles(RoleChecker checker) throws SecurityException {
+
+    }
+
+    @Override
+    public String invoke(File f, VirtualChannel channel) throws IOException {
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(f));
+        ZipEntry zipEntry = zis.getNextEntry();
+        String sbomgenPath = "";
+        while (zipEntry != null) {
+            File newFile = newFile(new File(destinationPath), zipEntry);
+            if (zipEntry.getName().endsWith("inspector-sbomgen")) {
+                sbomgenPath = newFile.getAbsolutePath();
+                newFile.setExecutable(true);
+            }
+
+            if (zipEntry.isDirectory()) {
+                if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                    throw new IOException("Failed to create directory " + newFile);
+                }
+            } else {
+                File parent = newFile.getParentFile();
+                if (!parent.isDirectory() && !parent.mkdirs()) {
+                    throw new IOException("Failed to create directory " + parent);
+                }
+
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+            }
+            zipEntry = zis.getNextEntry();
+        }
+
+        zis.closeEntry();
+        zis.close();
+
+
+        return sbomgenPath;
+    }
+
     private static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
         File destFile = new File(destinationDir, zipEntry.getName());
 
@@ -64,59 +124,5 @@ public class SbomgenDownloader {
         }
 
         return destFile;
-    }
-
-
-    @SuppressFBWarnings()
-    private static String unzipFile(FilePath zip) throws IOException, InterruptedException, ExecutionException {
-        FilePath destination = zip.getParent().child(zip.getRemote().replace(".zip", ""));
-        Future<String> callable = zip.actAsync(new FilePath.FileCallable<String>() {
-            @Override
-            public void checkRoles(RoleChecker checker) throws SecurityException {
-
-            }
-
-            @Override
-            public String invoke(File f, VirtualChannel channel) throws IOException {
-                byte[] buffer = new byte[1024];
-                ZipInputStream zis = new ZipInputStream(new FileInputStream(f));
-                ZipEntry zipEntry = zis.getNextEntry();
-                String sbomgenPath = "";
-                while (zipEntry != null) {
-                    File newFile = newFile(new File(destination.getRemote()), zipEntry);
-                    if (zipEntry.getName().endsWith("inspector-sbomgen")) {
-                        sbomgenPath = newFile.getAbsolutePath();
-                        newFile.setExecutable(true);
-                    }
-
-                    if (zipEntry.isDirectory()) {
-                        if (!newFile.isDirectory() && !newFile.mkdirs()) {
-                            throw new IOException("Failed to create directory " + newFile);
-                        }
-                    } else {
-                        File parent = newFile.getParentFile();
-                        if (!parent.isDirectory() && !parent.mkdirs()) {
-                            throw new IOException("Failed to create directory " + parent);
-                        }
-
-                        FileOutputStream fos = new FileOutputStream(newFile);
-                        int len;
-                        while ((len = zis.read(buffer)) > 0) {
-                            fos.write(buffer, 0, len);
-                        }
-                        fos.close();
-                    }
-                    zipEntry = zis.getNextEntry();
-                }
-
-                zis.closeEntry();
-                zis.close();
-
-
-                return sbomgenPath;
-            }
-        });
-
-        return callable.get();
     }
 }
