10d457b39320d855f1f1c82d9318e9a4c0bd802c
waltwilo <144280698+waltwilo@users.noreply.github.com>
2024-10-14 10:39:26 -0600

    Update GHATest.py

--- .github/workflows/GHATest.py
+++ .github/workflows/GHATest.py
@@ -12,7 +12,7 @@ parser.add_argument('-n')
 args = parser.parse_args()
 print(args)
 
-test = subprocess.run(["ssh", "-o", "StrictHostKeyChecking=no", "-i", "~/.ssh/id_rsa", "-l", args.l, "-p", args.p, args.a, "build", args.n, "-s"], capture_output=True, text=True) 
+test = subprocess.run(["ssh", "-o", "StrictHostKeyChecking=no", "-i", "~/.ssh/id_rsa", "-l", args.l, "-p", args.p, args.a, "build", args.n, "-s", "-v"], capture_output=True, text=True) 
 print(test.stdout)
 
 if "SUCCESS" not in test.stdout:
