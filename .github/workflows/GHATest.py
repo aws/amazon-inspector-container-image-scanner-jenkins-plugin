4096625afe336caeb9ac332ef9d301d0653fab81
waltwilo <144280698+waltwilo@users.noreply.github.com>
2024-10-14 13:11:36 -0600

    Update GHATest.py

--- .github/workflows/GHATest.py
+++ .github/workflows/GHATest.py
@@ -13,6 +13,6 @@ print("RUNNING")
 args = parser.parse_args()
 print(args)            
 
-test = subprocess.run(f"ssh -o StrictHostKeyChecking=no -i ~/.ssh/jenkinslab.pem -l {args.l} -p {args.p} {args.a} build {args.n} -s -v", shell=True)
+test = subprocess.run(f"ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -l {args.l} -p {args.p} {args.a} build {args.n} -s -v", shell=True)
 print(f"Ending test with exit code {test.returncode}")
 exit(test.returncode)
